5cbc42b0e923873f3ad90aa9eb21dd0e19ae01d2
Fe <you@example.com>
2024-12-19 13:43:04 -0500

    step 5

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -47,6 +47,14 @@ def step_impl(context):
     # load the database with new products
     #
     for row in context.table:
-        #
-        # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
-        #
+        for row in context.table:
+            info = {
+                "name": row['name'],
+                "description": row['description'],
+                "price": row['price'],
+                "available": row['available'] in ['True', 'true', '1'],
+                "category": row['category']
+            } 
+
+            response = requests.post(rest_endpoint, json = info)
+            assert response.status_code = HTTP_200_OK
