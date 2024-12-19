c8ac0c1d358b6b51d65c2f9cd65c5c56db29d525
Fe <you@example.com>
2024-12-19 14:06:56 -0500

    step 5

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -105,6 +105,18 @@ def step_impl(context, element_name):
 ##################################################################
 
 ## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click() 
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element_value((By.ID, 'search_results'), name)
+    )
+
+    assert(found)
 
 ##################################################################
 # This code works because of the following naming convention:
